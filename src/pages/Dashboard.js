7dbaeaa35df5ebecfa98c3891c4ce2e91161d5b7
uaalumni02 <dbell1911@gmail.com>
2024-10-30 18:58:26 -0500

    trying to fix delete

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -87,8 +87,7 @@ const Dashboard = () => {
     })
       .then((res) => res.json())
       .then((response) => {
-        console.log(response.data[0].dueDate)
-        setDateToEdit(response.data[0].dueDate)
+        setDateToEdit(response.data[0].dueDate);
         const fetchedTasks = response.data;
         setTasks(fetchedTasks);
         updateChartData(fetchedTasks);
@@ -264,8 +263,7 @@ const Dashboard = () => {
         .then((response) => {
           if (response.success) {
             fetchUserData(); // Refresh the task list after deletion
-          } else {
-            console.error("Error deleting task:", response.message);
+            setShowStatusModal(false); // Close the status modal
           }
         })
         .catch((error) => console.error("Error:", error));
@@ -296,43 +294,39 @@ const Dashboard = () => {
     return matchesDueDate && matchesStatus && matchesCategory;
   });
 
- 
-const formattedDate = new Date(dateToEdit * 1000); // Convert to milliseconds
-console.log("Formatted date:", formattedDate);
+  const formattedDate = new Date(dateToEdit * 1000); // Convert to milliseconds
 
-const dueDate = `${(formattedDate.getMonth() + 1)
-  .toString()
-  .padStart(2, "0")}-${formattedDate
-  .getDate()
-  .toString()
-  .padStart(2, "0")}-${formattedDate.getFullYear().toString().slice(-2)}`;
+  const dueDate = `${(formattedDate.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${formattedDate
+    .getDate()
+    .toString()
+    .padStart(2, "0")}-${formattedDate.getFullYear().toString().slice(-2)}`;
 
-const handleUpdateStatus = (taskId) => {
-  fetch(`http://localhost:3000/api/task/${taskId}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-    body: JSON.stringify({
-      task: taskToEdit,
-      userName: userToEdit,
-      dueDate: dueDate, // Use the formatted due date
-      status: selectedStatusId,
-    }),
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      if (response.success) {
-        fetchUserData(); // Refresh the task list after updating
-        setShowStatusModal(false); // Close the status modal
-      } else {
-        console.error("Error updating task status:", response.message);
-      }
+  const handleUpdateStatus = (taskId) => {
+    fetch(`http://localhost:3000/api/task/${taskId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        task: taskToEdit,
+        userName: userToEdit,
+        dueDate: dueDate, // Use the formatted due date
+        status: selectedStatusId,
+      }),
     })
-    .catch((error) => console.error("Error:", error));
-};
-
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        if (response.success) {
+          fetchUserData(); // Refresh the task list after updating
+          setShowStatusModal(false); // Close the status modal
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  };
 
   return (
     <Container>
